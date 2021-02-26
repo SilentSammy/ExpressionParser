6c02b1525382f147d118b247658610da3ff994fe
Samuel Cabrera <scabrera197@gmail.com>
2021-02-26 08:21:03 -0500

    first commit

---
+++ ExpressionParser/ExpressionParser.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+
+namespace ExpressionParser
+{
+    public class ExpressionParser
+    {
+        static readonly DataTable computer = new DataTable();
+        static readonly Dictionary<string, string> operators = new Dictionary<string, string>()
+        {
+            ["=="] = "=",
+            ["!="] = "<>",
+            ["&&"] = "AND",
+            ["||"] = "OR",
+        };
+        public virtual IDictionary<string, object> Variables { get; }
+        public virtual object GetVariable(string name) =>
+            Variables[name];
+        protected virtual IEnumerable<string> FindVariables(string expression) =>
+            Variables.Keys.Where(k => expression.Contains(k));
+        public ExpressionParser(IDictionary<string, object> variables = null) =>
+            Variables = variables ?? new Dictionary<string, object>();
+        public object Compute(string expression)
+        {
+            StringBuilder sb = new StringBuilder(expression);
+            foreach (var key in FindVariables(expression))
+                sb.Replace(key, GetVariable(key).ToString());
+            foreach (var o in operators)
+                sb.Replace(o.Key, o.Value);
+            return computer.Compute(sb.ToString(), null);
+        }
+    }
+}
---
+++ JExpressionParser/JExpressionParser.cs
@@ -0,0 +1,36 @@
+﻿using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ExpressionParser
+{
+    public class JExpressionParser : ExpressionParser
+    {
+        const string self = ". ";
+        public JToken JVariables { get; set; }
+        public override object GetVariable(string name) => name == self ? JVariables : JVariables.SelectToken(name);
+        static List<string> GetAllPaths(JToken token, bool isBaseToken = false)
+        {
+            var list = new List<string>();
+            if (token != null)
+            {
+                foreach (var item in token.Children())
+                {
+                    if (item.HasValues) list.AddRange(GetAllPaths(item));
+                    else list.Add(item.Path);
+                }
+                if (isBaseToken && token.Parent != null)
+                    for (int i = 0; i < list.Count; i++)
+                        list[i] = list[i].Substring(token.Path.Length + 1);
+                if (isBaseToken) list.Add(self);
+            }
+            return list;
+        }
+        protected override IEnumerable<string> FindVariables(string expression) =>
+            GetAllPaths(JVariables, true).Where(p => expression.Contains(p));
+        public JExpressionParser(JToken jVariables = null)
+        {
+            JVariables = jVariables ?? new JObject();
+        }
+    }
+}
